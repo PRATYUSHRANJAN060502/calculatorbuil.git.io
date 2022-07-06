d3b14ab519f41a94b4dfae50cfac4dd5a00b674e
pratyush ranjan <82261854+PRATYUSHRANJAN060502@users.noreply.github.com>
2022-07-06 10:15:36 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,34 @@
+let num1=prompt("what is your no.")
+let num2=prompt("what is your another no")
+document.getElementById("num1-el").textContent=num1
+document.getElementById("num2-el").textContent=num2
+ sumEl=document.getElementById("sum-el")
+
+function add()
+{
+let sum1=num1-(-num2)
+sumEl.textContent="sum: "+sum1
+}
+
+
+function subtract(){
+let sum1=num1-num2
+sumEl.textContent="sum: "+sum1
+
+}
+
+
+function multiply(){
+let sum1=num1*num2
+sumEl.textContent="sum: "+sum1
+
+}
+
+function divide(){
+let sum1=num1/num2
+sumEl.textContent="sum: "+sum1
+
+}
+
+
+
